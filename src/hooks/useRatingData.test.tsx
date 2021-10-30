66e2a7ffb394dbb675df6c887114ab6521681d85
buteninvania <buteninvania@gmail.com>
2021-10-30 22:26:38 +0300

    Refactored the Rating component, wrote tests for components and hooks

--- src/hooks/useRatingData.test.tsx
+++ src/hooks/useRatingData.test.tsx
@@ -1,6 +1,7 @@
 import { renderHook, act } from '@testing-library/react-hooks'
 import useRatingData from './useRatingData'
 
-test('Should return an array of ratings in the given order', () => {
+test('Should return an array of rating data, in the given order', () => {
   const {result} = renderHook(() => useRatingData(3))
+  expect(result.current).toStrictEqual([true, true, true, false, false])
 })
