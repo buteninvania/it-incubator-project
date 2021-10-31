cbb2752511f5c99c9e0026ab079c015b85c443f9
buteninvania <buteninvania@gmail.com>
2021-10-31 19:31:12 +0300

    Added functionality for the accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,38 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Accordion(props: AccordionPropsType) {
+
+    const [isShow, setIsShow] = useState(false)
+
+    const clickHundler = () => {
+        setIsShow(!isShow)
+    }
+
     return (
         <div>
-            <AccordionTitle title={props.title}/>
-            <AccordionBody />
+            <AccordionTitle isShow={clickHundler} title={props.title}/>
+            <AccordionBody isShow={isShow}/>
         </div>
     );
 }
 
 function AccordionTitle(props: AccordionTitleDataProps) {
     return (
-        <h3>
+        <h3 onClick={() => props.isShow()}>
             Меню
         </h3>
     );
 }
 
-function AccordionBody() {
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
-    );
+function AccordionBody(props:AccordionBodyPropsType) {
+
+    if(props.isShow) {
+        return (
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        );
+    } else {
+        return <></>
+    }
 }
 
 export default Accordion;
 
+type AccordionBodyPropsType = {
+    isShow: boolean
+}
 type AccordionPropsType = {
     title: string
 }
 type AccordionTitleDataProps = {
     title: string
+    isShow: () => void
 }
